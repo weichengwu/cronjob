202294c75800e3fc0d75adef2000247ad329d137
stephenw <zhilong.wang@ele.me>
2016-11-02 14:14:56 +0800

    add receiever

--- sendMail.py
+++ sendMail.py
@@ -5,7 +5,8 @@ def send_message(html):
         "https://api.mailgun.net/v3/noreply.stephenw.cc/messages",
         auth=("api", "key-75f9cf9639283b1080e762c9d9bd87db"),
         data={"from": "PI机器人 <noreply@stephenw.cc>",
-              "to": ["zhilong.wang@ele.me", "wuvcen@gmail.com"],
+              "to": ["zhilong.wang@ele.me",
+                  "wuvcen@gmail.com", "fattycat.u@gmail.com"],
               "subject": "PI日报",
               "text": "sorry, some error happened",
               "html": html})
